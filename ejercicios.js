4fd09c2dd158dc3ab94694aec7dfe148f122869c
Emilio Masciarelli <emiliomasciarelli@gmail.com>
2023-07-14 16:57:10 -0300

    se agregaron mas ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,6 @@
+/*
+
+
 // Ejercicio 1, crear variable nombre y edad e imprimir en consola.
 let nombre = 'Emilio';
 let edad = 35;
@@ -16,4 +19,30 @@ console.log(`Hola! Mi nombre es ${nombre} y tengo ${edad} años.`);
  let num1 = 8;
  let num2 = 10;
  console.log(num1 + num2);
+*/
+
+
+ /* Ejercicio 5, el siguiente codigo nos esta dando un error en consola. Corregir para que de 25 
+ const numero = 15;
+ numero = 25;
+ console.log(numero);
  
+let numero = 15;
+numero = 25;
+console.log(numero);
+
+// Ejercicio 6, escribi un codigo que pase el valor de una variable como una alerta en el navegador.
+let alerta = 'Esto es una alerta';
+alert(alerta);
+
+// Ejercicio 7, escribi un codigo que pida un valor, lo almacene en una variable y lo muestre por consola.
+let valorIngresado = prompt('Ingresa tu nombre');
+console.log(valorIngresado);
+
+// Ejercicio 8, igual que el ejercicio anterior pero mostrar una alerta diciendo: "El valor ingresado es (el valor)".
+let valor = prompt('Ingresa un numero del 1 al 10');
+alert(`El valor ingresado es ${valor}`);
+
+*/
+
+// Ejercicio 9, escribir un codigo que pida dos nombres, almacenar en variables y mostrar alerta que diga: "El primer valor ingresado es (valor1) y el segundo es (valor2)".
