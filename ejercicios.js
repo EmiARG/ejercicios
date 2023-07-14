3fc9673ff3a02db60155ade862116a6db7ed9229
Emilio Masciarelli <emiliomasciarelli@gmail.com>
2023-07-14 18:02:17 -0300

    Hasta ej 14

--- ejercicios.js
+++ ejercicios.js
@@ -44,5 +44,45 @@ let valor = prompt('Ingresa un numero del 1 al 10');
 alert(`El valor ingresado es ${valor}`);
 
 */
-
+/*
 // Ejercicio 9, escribir un codigo que pida dos nombres, almacenar en variables y mostrar alerta que diga: "El primer valor ingresado es (valor1) y el segundo es (valor2)".
+ let nom1 = prompt('valor 1');
+ let nom2 = prompt('Valor 2');
+ alert(`El primer valor ingresado es ${nom1} y el segundo es ${nom2}`);
+ */
+
+// Ejercicio 10, El siguiente codigo nos devuelve "undefined" por consola. Modificalo de manera que nos devuelva el valor de la variable nombre.
+ // console.log(nombre);
+ // var nombre = "Carlos";
+ var nombre = "Carlos";
+ console.log(nombre);
+ 
+// Ejercicio 11, teniendo en cuenta lo visto en clase scopes, que es lo que nos va a imprimir la consola en el siguiente ejercicio?
+/* let valor = 'Hola';
+    function mostrarVariable(){
+        let valor = 'chau';
+    }
+    console.log(valor);
+    Va a imprimir Hola.
+*/ 
+// Ejercicio 12, Que va a imprimir la consola en el siguiente ejemplo.
+// let a = false, let b = true, let c = true
+// console.log(!a && b && c);
+// La consola va a imprimir TRUE;
+
+// Ejercicio 13, que se va a imprimir en el sig ejercicio
+// let a = 18, let b = 20
+// console.log( a < b || a === b || a >= b);
+// La consola va a imprimir True;
+
+// Ejercicio 14, escribi un codigo que tome una variable edad como un valor numerico e imprima por consola "soy mayor de edad" si el numero es mayor o igual a 18, y "soy menor de edad" en caso contrario.
+/*
+let edad = 25;
+if (edad >= 18){
+    console.log('Soy mayor de edad');
+} else {
+    console.log('Soy menor de edad')
+};
+*/
+
+// Ejercicio 15, 
