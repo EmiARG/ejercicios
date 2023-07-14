215db7052ce158083d72e2896a2b56284973b2eb
Emilio Masciarelli <emiliomasciarelli@gmail.com>
2023-07-14 16:37:08 -0300

    Se agregaron 2 ejercicios mas

--- ejercicios.js
+++ ejercicios.js
@@ -1,8 +1,19 @@
 // Ejercicio 1, crear variable nombre y edad e imprimir en consola.
-const nombre = 'Emilio';
-const edad = 35;
+let nombre = 'Emilio';
+let edad = 35;
 console.log(nombre, edad);
 
 // Ejercicio 2, imprimi en consola "Hola mi nombre es (nombre) y tengo (edad) años".
 console.log('Hola! Mi nombre es ' + nombre + ' y tengo ' + edad + ' años.');
 console.log(`Hola! Mi nombre es ${nombre} y tengo ${edad} años.`);
+
+ // Ejercicio 3, reasigna los valores de las variables e imprime en consola.
+ nombre = 'Andres';
+ edad = 29;
+ console.log(`Hola! Mi nombre es ${nombre} y tengo ${edad} años.`);
+
+ // Ejercicio 4, crea dos variables con valores de tipo numero e imprimi la suma de ambos por consola.
+ let num1 = 8;
+ let num2 = 10;
+ console.log(num1 + num2);
+ 
