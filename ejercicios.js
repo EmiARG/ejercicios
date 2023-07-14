c3087a9718eb7cf1fe0f31e6aa5b35fdffc4d31a
Emilio Masciarelli <emiliomasciarelli@gmail.com>
2023-07-14 16:24:21 -0300

    Primer commit

---
+++ ejercicios.js
@@ -0,0 +1,8 @@
+// Ejercicio 1, crear variable nombre y edad e imprimir en consola.
+const nombre = 'Emilio';
+const edad = 35;
+console.log(nombre, edad);
+
+// Ejercicio 2, imprimi en consola "Hola mi nombre es (nombre) y tengo (edad) años".
+console.log('Hola! Mi nombre es ' + nombre + ' y tengo ' + edad + ' años.');
+console.log(`Hola! Mi nombre es ${nombre} y tengo ${edad} años.`);
