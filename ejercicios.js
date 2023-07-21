77666fd017eb2b402ef8b10f575d3af7c10145d1
Emilio Masciarelli <emiliomasciarelli@gmail.com>
2023-07-21 20:39:33 -0300

    descanso

--- ejercicios.js
+++ ejercicios.js
@@ -559,4 +559,31 @@ let funcion = (arr, numero, callback) => {
 console.log(funcion(numeros, 2, suma));
 */
 
-//
+/* Ejercicio 44, A partir de este ejercicio vamos a trabajar con objetos. Les damos la clase y también 5 películas ya creadas (vamos a usar este array para los siguientes ejercicios también).
+Como primera medida, creá un array de películas vacío y mediante métodos de array agrega todas las películas al array vacío e imprimí por consola el array.
+*/
+
+class Pelicula {
+    constructor(titulo, duracion, año) {
+      this.titulo = titulo;
+      this.duracion = duracion;
+      this.año = año;
+    }
+  }
+  
+  const pokemon = new Pelicula("Pokemon:La Pelicula", 75, 1998);
+  const avengers = new Pelicula("Avengers: Endgame", 181, 2019);
+  const starWars = new Pelicula(
+    "Star Wars: El despertar de la fuerza",
+    135,
+    2015
+  );
+  const batman = new Pelicula("Batman vs Superman", 151, 2016);
+  const wonderWoman = new Pelicula("La Mujer Maravilla", 141, 2017);
+
+  let arrPeliculas = [];
+  arrPeliculas.push(pokemon, avengers, starWars, batman, wonderWoman);
+  console.log(arrPeliculas);
+
+  // Creá una función que ordene el array por año de salida, desde la mas larga hasta la mas corta e imprimí el array por consola.
+
